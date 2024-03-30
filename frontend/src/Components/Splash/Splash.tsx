cb815f4eade0994e72655f2563bf52ba145d1f24
nj <nickgentrymma@gmail.com>
2024-03-30 00:00:10 -0700

    added quote rough styled.

--- frontend/src/Components/Splash/Splash.tsx
+++ frontend/src/Components/Splash/Splash.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Splash.css';
 
 function Splash(): JSX.Element {
     return (
         <div className='splash-wrapper'>
-            Hello From Splash.
+            <div className='quote-wrapper'>
+                <h1 className='quote'>
+                    "As designers, we learn from the ever changing world around us. We constantly evolve our actions and abilities to be continued."
+                    <p className='author'>-Katarina Gentry</p>
+                </h1>
+            </div>
         </div>
     );
 };
