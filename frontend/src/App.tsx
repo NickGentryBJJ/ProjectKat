aaa496982baf6cf558afb8eea843db3d9a32adfc
nj <nickgentrymma@gmail.com>
2024-03-29 23:37:32 -0700

    touched up navbar and added side menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import Splash from './Components/Splash/Splash';
 import Footer from './Components/Footer/Footer';
 import NavBar from './Components/NavBar/NavBar';
+import SideMenu from './Components/NavBar/SideMenu/SideMenu';
 
 function App(): JSX.Element {
   const [open, setOpen] = useState<boolean>(false);
   return (
   <>
     <NavBar open={open} setOpen={setOpen}/>
+    <SideMenu open={open} setOpen={setOpen}/>
     <Splash/>
     <Footer/>
   </>
