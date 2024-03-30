772bf038ba90a487003ac3170d1459258afc42db
nj <nickgentrymma@gmail.com>
2024-03-29 23:48:25 -0700

    added footer

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer(): JSX.Element {
     return (
-        <footer className='footer-wrapper'>
-            Hello From Footer.
-        </footer>
+    <footer id="contact-section">
+        <div className='footer-container'>
+            <h2 className='thank-you'>
+                Thank you for visiting!
+            </h2>
+        </div>
+    </footer> 
     );
 };
 export default Footer;
