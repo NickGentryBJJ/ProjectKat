6e7b8ce2eb3d8e2b9b3584219a5ac7bad2cb4a40
nj <nickgentrymma@gmail.com>
2024-03-31 01:13:04 -0700

    adjusted splash, navbar

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -9,15 +9,46 @@ interface NavProps {
 
 function NavBar(props: NavProps): JSX.Element {
     const {open, setOpen} = props;
+    const works: Record<string, string> = {
+        "CDA WORK": "cda",
+        "PLANET ART WORK": "paw",
+        "PACIFICA SENIOR LIVING": "psl",
+        "WORK COMPLETED IN SCHOOL": "wcis"
+    }
     function keniBurke(): void { // Risin To The Top!
         const body = document.documentElement || document.body;
         body.scrollIntoView({ behavior: 'smooth', block: 'start' });
         setOpen(false);
     };
+    function handleHover(id: string): void {
+        const newSplash = document.getElementById(id);
+        if (newSplash){
+            newSplash.classList.remove("hidden");
+        };
+    };
+    function handleLeave(id:string) {
+        const pSplash = document.getElementById(id);
+        if (pSplash){
+            pSplash.classList.add("hidden");
+        };
+    };
     return (
         <div className='nav-wrapper'>
             <div className='nav-left'>
                 <img className='nav-logo' onClick={keniBurke} src={logo} alt="Katarina Gentry" />
+            </div>
+            <div className='nav-mid'>
+                {Object.entries(works).map(([name, value], index) => (
+                    <div 
+                        onMouseLeave={() => handleLeave(value)} 
+                        onMouseEnter={() => handleHover(value)} 
+                        key={index} 
+                        className='work-item-wrapper'>
+                            <h3 className='work-item'>
+                                {name}
+                            </h3>
+                    </div>
+                ))}
             </div>
             <div className='nav-right'>
                 <img className='nav-logo' onClick={() => setOpen(!open)} src={menu} alt="menu" />
