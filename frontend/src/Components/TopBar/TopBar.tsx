6501ccb7a7102f700e12cb45593e5bfcc07be016
nj <nickgentrymma@gmail.com>
2024-04-18 15:34:56 -0700

    adjusted lineheight in top bar

--- frontend/src/Components/TopBar/TopBar.tsx
+++ frontend/src/Components/TopBar/TopBar.tsx
@@ -5,7 +5,7 @@ function TopBar(): JSX.Element {
     return (
         <div className='top-bar-wrapper'>
             <div className='left-bar'>
-                <h1 className='top-text'>Katarina Gentry,</h1><br />
+                <h1 className='top-text'>Katarina Gentry,</h1>
                 <h1 className='top-text'>Graphic Design</h1>
             </div>
             <div className='right-bar'>
