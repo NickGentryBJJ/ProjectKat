968f8607ee2dd6578908096ebdc355b90b64603a
nj <nickgentrymma@gmail.com>
2024-04-19 16:35:21 -0700

    rough styled popout

--- frontend/src/Components/WorksSection/CDA/CdaMid/CdaMid.tsx
+++ frontend/src/Components/WorksSection/CDA/CdaMid/CdaMid.tsx
@@ -6,16 +6,10 @@ function CdaMid(): JSX.Element {
         <div className='mid-cda-wrapper'>
             <h1 className='mid-cda-title'>
                 CDA Showreel
-            </h1>
-            <p className='mid-cda-desc'>
-                Using After Effects and Illustrator to put together this video. I took lead on this project by coming up with the storyboard, organization and head editing of the video, and maintained the CDA brand messaging throughout. My coworker and videographer in the company created the video section edits and added sound design. Using teamwork to make appropriate edits, this video is currently the teams main marketing device.
-            </p>
-            <h1 className='mid-cda-title'>
-                Vol 5 Ebook in Data Privacy Changes.
-            </h1>
-            <p className='mid-cda-desc'>
-                For this ebook I wanted to play with space and leading lines. I used a luscious forest green against stark white to create contrast and make the information inside the focal point.
-            </p>            
+            </h1>    
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In tenetur corporis, consectetur aliquid perspiciatis totam. Assumenda velit cupiditate quod! Ut quam consequatur exercitationem nemo mollitia optio incidunt labore velit harum.    
+            </p>     
         </div>
     );
 };
