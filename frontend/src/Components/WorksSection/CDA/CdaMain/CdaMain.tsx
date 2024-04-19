f867bb3dd7eaccaa87c1f7a64236a6fd5866473c
nj <nickgentrymma@gmail.com>
2024-04-19 16:01:25 -0700

    rough styled popout section

--- frontend/src/Components/WorksSection/CDA/CdaMain/CdaMain.tsx
+++ frontend/src/Components/WorksSection/CDA/CdaMain/CdaMain.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './CdaMain.css';
 import CdaTop from '../CdaTop/CdaTop';
 import CdaMid from '../CdaMid/CdaMid';
+import image from '../../../../Images/linkedin_1384088.png'
 
 function CdaMain(): JSX.Element {
     return (
         <div className='main-cda-wrapper'>
-            <CdaTop/>
-            <CdaMid/>
+            <div className='cda-pop-img-wrap'>
+                <img className='cda-pop' src={image} alt="image" />
+            </div>
+            <div className='cda-pop-text-wrap'>
+                <CdaTop/>
+                <CdaMid/>
+            </div>
         </div>
     );
 };
--- frontend/src/Components/WorksSection/CDA/CdaMid/CdaMid.tsx
+++ frontend/src/Components/WorksSection/CDA/CdaMid/CdaMid.tsx
@@ -10,16 +10,13 @@ function CdaMid(): JSX.Element {
             <p className='mid-cda-desc'>
                 Using After Effects and Illustrator to put together this video. I took lead on this project by coming up with the storyboard, organization and head editing of the video, and maintained the CDA brand messaging throughout. My coworker and videographer in the company created the video section edits and added sound design. Using teamwork to make appropriate edits, this video is currently the teams main marketing device.
             </p>
-            {/* video */}
             <h1 className='mid-cda-title'>
                 Vol 5 Ebook in Data Privacy Changes.
             </h1>
             <p className='mid-cda-desc'>
                 For this ebook I wanted to play with space and leading lines. I used a luscious forest green against stark white to create contrast and make the information inside the focal point.
-            </p>
-            {/* flipbook */}
-            
+            </p>            
         </div>
     );
-}
+};
 export default CdaMid;
