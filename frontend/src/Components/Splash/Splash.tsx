aa88d636a3d6b15fc284e823a6fa1c34689e06a8
nj <nickgentrymma@gmail.com>
2024-05-29 19:45:12 -0700

    fixed quote sizing

--- frontend/src/Components/Splash/Splash.tsx
+++ frontend/src/Components/Splash/Splash.tsx
@@ -6,14 +6,31 @@ function Splash(): JSX.Element {
     return (
         <div  className='splash-wrapper'>
             <div id='sw' className='quote-wrapper'>
-                <h1 className='quote'>
-                <p className='q-text'>
-                    &nbsp;"As designers, we learn from the&nbsp;<br/>&nbsp;ever changing world around us.&nbsp;<br/>&nbsp;We constantly evolve our actions&nbsp;<br/>&nbsp;and abilities to be continued."&nbsp;
-                </p><br/>
+                <span className='q-text'>
+                    &nbsp;"As designers, we learn from the&nbsp;<br/>
+                </span>
+                <span className='q-text'>
+                    &nbsp;ever changing world around us.&nbsp;<br/>
+                </span>
+                <span className='q-text'>
+                    &nbsp;We constantly evolve our actions&nbsp;<br/>
+                </span>
+                <span className='q-text'>
+                    &nbsp;and abilities to be continued."&nbsp;<br/>
+                </span>
                     <p className='author'>&nbsp;-Katarina Gentry&nbsp;</p>
-                </h1>
             </div>
         </div>
+        // <div  className='splash-wrapper'>
+        //     <div id='sw' className='quote-wrapper'>
+        //         <h1 className='quote'>
+        //         <p className='q-text'>
+        //             &nbsp;"As designers, we learn from the&nbsp;<br/>&nbsp;ever changing world around us.&nbsp;<br/>&nbsp;We constantly evolve our actions&nbsp;<br/>&nbsp;and abilities to be continued."&nbsp;
+        //         </p><br/>
+        //             <p className='author'>&nbsp;-Katarina Gentry&nbsp;</p>
+        //         </h1>
+        //     </div>
+        // </div>
     );
 };
 export default Splash;
